4b4b99b0cd5216a4aa350fb381e4c0eb5dd1e8f5
Kartikey Tanna <tannakartikey@gmail.com>
2020-08-18 22:07:58 +0530

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sweta Upadhyay`,
+    description: `Sweta Upadhyay's personal webpage`,
+    author: `Sweta Upadhyay`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
