22a16758f4704b48eb8ce68bf3b4f6495f97bc1a
Kartikey Tanna <tannakartikey@gmail.com>
2020-08-18 21:50:22 +0530

    Changed the image

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "sweta_upadhyay.png" }) {
+      placeholderImage: file(relativePath: { eq: "sweta_new.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
